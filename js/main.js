6f31664314c48e833143afdd3a5feaf58315d5e1
Colin Tianying Chen <colinchen@Beethoven.local>
2016-06-06 17:21:13 -0700

    Font adjustment

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@
   var smscene1 = new ScrollMagic.Scene({duration: 1500})
     .setTween("#wave", {y:800})
     .addTo(controller);
-    
+
 
 
   var tweenHeroFade = new TweenMax.fromTo(".hero-1", 1, {autoAlpha: 1}, {autoAlpha: 0, ease: Linear.easeNone});
-      
+
   var tweenHeroSlow = new TweenMax.fromTo(".hero-1", 1, {y: 0}, {y:250, ease: Linear.easeNone});
 
 
@@ -16,7 +16,7 @@
   var smscene3 = new ScrollMagic.Scene({duration: 400})
     .setTween(tweenHeroFade)
     .addTo(controller);
-          
+
   var smscene3a = new ScrollMagic.Scene({duration: 400})
     .setTween(tweenHeroSlow)
     .addTo(controller);
@@ -29,40 +29,40 @@
   	]);
 
   // build scene
-  var smscene3 = new ScrollMagic.Scene({triggerElement: "#about", duration: 400})
+  var smscene3 = new ScrollMagic.Scene({triggerElement: "#about", triggerHook: "onEnter", duration: 600})
     .setTween(tweenPort)
     .addTo(controller);
 
   var changeColor = new TimelineMax ()
     .add([
       TweenMax.fromTo(".portfolio-container", 1, {autoAlpha: 1}, {autoAlpha: 0, ease: Linear.easeNone}),
-      TweenMax.to('.navItem', 1.5, {color: '#aaa'})
+      TweenMax.to('.navItem', 1.5, {color: '#ccc'})
     ]);
 
-  var smsceneChangeColor = new ScrollMagic.Scene({triggerElement: "#about", duration: 400})
+  var smsceneChangeColor = new ScrollMagic.Scene({triggerElement: "#about", duration: 300})
     .setTween(changeColor)
     .addTo(controller);
-    
+
   var portraitFade = new TimelineMax ()
     .add([
       TweenMax.to("#fadeBox", 1, {autoAlpha: 1, ease: Linear.easeNone}),
     ]);
-    
+
   var smscenePortraitFade = new ScrollMagic.Scene({triggerElement: "#about", triggerHook: 'onLeave', offset: -200, duration: 200})
     .setTween(portraitFade)
     .addTo(controller);
-    
+
   var portraitFadeOut = new TweenMax.to("#fadeBox", 1, {autoAlpha: 0});
-    
+
   var smsenePortraitFadeOut = new ScrollMagic.Scene({triggerElement: "#fadeTrigger", duration: 200})
     .setTween(portraitFadeOut)
     .addTo(controller);
-  
+
   var contactIn = new TimelineMax ()
     .add([
       TweenMax.fromTo(".contact-container", 1, {autoAlpha: 0}, {autoAlpha: 1}),
       TweenMax.to("#logo_small", 1, {autoAlpha: 0, duration: 100})
-      
+
     ]);
 
   var smseneContactIn = new ScrollMagic.Scene({triggerElement: "#contact", duration: 200, reverse:true})
@@ -70,32 +70,32 @@
     .addTo(controller);
 
 
-  
-  
+
+
   var smsceneLogo = new ScrollMagic.Scene({triggerElement: ".portfolio-container"})
     // trigger a velocity opaticy animation
     .setVelocity("#logo_small", {opacity: 1}, {duration: 200, easing:Elastic})
     .addTo(controller);
-  
+
   var smsceneDivider2 = new ScrollMagic.Scene({triggerElement: "#portfolio", duration:50})
     // trigger a velocity opaticy animation
     .setTween(".divider", {y: "31rem"})
     .addTo(controller);
-            
+
   var smsceneDivider3 = new ScrollMagic.Scene({triggerElement: "#about", duration: 50})
     // trigger a velocity opaticy animation
     .setTween(".divider", {y: "61rem"})
     .addTo(controller);
-  
+
   var smsceneDivider4 = new ScrollMagic.Scene({triggerElement: "#contact", duration: 50})
     // trigger a velocity opaticy animation
     .setTween(".divider", {y: "91rem"})
     .addTo(controller);
-  
+
 
 
   //MISC FUNCTIONS
-  
+
   function resizePortrait(){
     if (parseFloat($(window).height()) / $(window).width() < 0.75){
       $('#portrait').css({'height':'80vh', 'width':'auto', 'left':'7vw'});
@@ -103,12 +103,12 @@
       $('#portrait').css({'height':'auto', 'width':'40vw', 'left':'0'});
     }
   };
-  
+
   function verticalAlignText(element){
     height = parseFloat($(window).height() - $(element).height())/2.0;
     $(element).css('top', height);
   }
-  
+
   function sizePortWidth(){
     portW = $(".portfolio-container").width();
     if (portW >= 720) {
@@ -119,15 +119,15 @@
       $(".gridItem").css({'width': '50vw', 'display':'block', 'margin': 'auto'});
     }
   }
-  
+
   //WINDOW AND DOCUMENT FUNCTIONS
-  
+
   $( window ).on( 'load', function(){
     $( '.load-screen' ).fadeOut( 'slow', function(){
       $( this ).remove();
     });
   });
-  
+
   $(document).ready(function(){
     $('body').smoothScroll({
       delegateSelector: 'ul a'
@@ -136,39 +136,46 @@
     resizePortrait();
     verticalAlignText('.aboutText1');
   });
-  
+
   $(window).on('resize', function(){
     sizePortWidth();
     resizePortrait();
     verticalAlignText('.aboutText1');
   });
-  
-  
+
+
   $(window).scroll(function(){
     if($(document).scrollTop() > 300){
       $('#wave').css({'display': 'none'});
     } else {
       $('#wave').css({'display': 'inherit'});
     };
-    
+
     if($(document).scrollTop() > 200){
       $('canvas').fadeOut("slow");
     } else {
       $('canvas').fadeIn("slow");
     }
   });
-  
-  
+
+  $(".gallery img").on('mouseenter', function(){
+    $(this).fadeTo('fast',0.3);
+
+  });
+
+  $(".gallery img").on('mouseleave', function(){
+    $(this).fadeTo('fast',1);
+
+  });
+
+
   $(".filter").on('click', function(){
     $(".filter").css({'border-bottom':'none'});
     $(this).css({'border-bottom':'1px solid #c33'});
   });
-  
-  
+
+
 
   $(function(){
     $('#Container').mixItUp();
   });
-
-  
-    
